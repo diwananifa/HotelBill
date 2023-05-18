c83611fdd13ebdbf3f640da641010a5b15d4b3b9
Kanniyappan1992 <spkanikings@gmail.com>
2023-05-18 11:15:03 +0530

    customer details updated

--- src/main/java/com/hotel/bill/repository/CustomerRepo.java
+++ src/main/java/com/hotel/bill/repository/CustomerRepo.java
@@ -1,9 +1,10 @@
 package com.hotel.bill.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.hotel.bill.dto.Customer;
 
 public interface CustomerRepo extends JpaRepository<Customer, Integer> {
-
+	
+	//Customer findByPhone(Long phoneno);
+	//Customer findByName(String name);
 }
